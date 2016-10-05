9e8680255fb13da94d43b9c5941361fdc48705cd
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-05 11:24:19 +0200

    First version of coding completed.

--- src/main/java/project/canbalance/classes/CanBalance.java
+++ src/main/java/project/canbalance/classes/CanBalance.java
@@ -1,9 +1,36 @@
 package project.canbalance.classes;
+import java.util.Arrays;
 
 public class CanBalance {
 	
-	public boolean canBalance(int[] nums){
-		return true;
+	private boolean checkSums(int[] first, int[] last){
+		  int sumFirst = 0;
+		  int sumLast = 0;
+		  
+		  for(int i = 0; i < first.length; i++){
+		    sumFirst += first[i];
+		  }
+		  
+		  for(int j = 0; j < last.length; j++){
+		    sumLast += last[j];
+		  }
+		  
+		  return (sumFirst == sumLast);
+
 	}
+
+
+	public boolean canBalance(int[] nums) {
+	  int lastIndex = nums.length -1;
+	  boolean isSameSum = false;
+
+	  for(int i = 0; i < lastIndex; i++){ 
+	    isSameSum = checkSums(Arrays.copyOfRange(nums,0,i), Arrays.copyOfRange(nums,i+1,lastIndex));
+	    if (isSameSum)
+	      return isSameSum;
+	  }
+	  return isSameSum;
+	}
+		
 	
 }
